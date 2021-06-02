d5a7b128698e19d1ee5b0d67ac2108749ca832f0
TrungyuD <shawnkool10@gmail.com>
2021-06-02 13:57:32 +0700

    done client

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,15 +1,35 @@
-import React from 'react'
-import {Container} from '@material-ui/core';
+ 
+import React from 'react';
+import { Container, Fab } from '@material-ui/core';
+import AddIcon from '@material-ui/icons/Add';
+import { useDispatch } from 'react-redux';
+
 import Header from '../components/header';
 import PostList from '../components/PostList';
+import useStyles from './styles';
+import { showModal } from '../redux/actions';
+import CreatePostModal from '../components/CreatePostModal';
 
-const HomePage = () => {
-    return (
-        <Container maxWidth="lg">
-            <Header />
-            <PostList/>
-        </Container>
-    )
+export default function HomePage() {
+  const classes = useStyles();
+  const dispatch = useDispatch();
+
+  const openCreatePostModal = React.useCallback(() => {
+    dispatch(showModal());
+  }, [dispatch]);
+
+  return (
+    <Container maxWidth='lg'>
+      <Header />
+      <PostList />
+      <CreatePostModal />
+      <Fab
+        color='primary'
+        className={classes.fab}
+        onClick={openCreatePostModal}
+      >
+        <AddIcon />
+      </Fab>
+    </Container>
+  );
 }
-
-export default HomePage
